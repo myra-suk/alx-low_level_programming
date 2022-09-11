8cdde604c18bc9fb2e8c2d9ad6943a89e81a52e0
myra-suk <myrasanaa17@gmail.com>
2022-09-11 12:40:25 +0300

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<studio.h>
+/**
+*main - Entry point 
+*
+*Return: Alwys 0 (Success)
+*/
+int main(void)
+{
+    puts("\"Programing is like building a multilingual puzzle");
+    return (0);
+}
