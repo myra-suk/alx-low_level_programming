3ce8c1bda517059964f0479d49ec60300f526e57
myra-suk <myrasanaa17@gmail.com>
2022-09-15 01:47:36 +0300

    Create 103-fibonacci.c
    Task 15. Print added value of even fibonacci numbers

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the added value of the even-valued
+ * fibonacci numbers
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+long int n1, n2, fn, efn;
+
+n1 = 1;
+n2 = 2;
+fn = efn = 0;
+while (fn <= 4000000)
+{
+fn = n1 + n2;
+n1 = n2;
+n2 = fn;
+if ((n1 % 2) == 0)
+{
+    efn += n1;
+}
+}
+printg("%ld\n", efn);
+return (0);
+}
