34be0a3a492f0b2f06faeebb3c0ae0197e8a87cc
myra-suk <myrasanaa17@gmail.com>
2022-09-14 22:57:20 +0300

    Create 10-add.c
    Task 10.Add Integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns
+ * the answer
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Answer of addition of the two integers
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
