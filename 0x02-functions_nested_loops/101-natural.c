04e0f3c2a296494b4d1b053fc4b01cf3e3f6feb3
myra-suk <myrasanaa17@gmail.com>
2022-09-15 01:03:02 +0300

    Update 101-natural.c
    Task 13.Print multiples of 3 or 5 less than 1024

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,7 @@
 #include  <stdio.h>
 
 /**
- * main- Prints natural numbers below 1024 that are
+ * main - Prints natural numbers below 1024 that are
  * multiples of 3 or 5
  *
  * Returns: Always 0 (Success)
