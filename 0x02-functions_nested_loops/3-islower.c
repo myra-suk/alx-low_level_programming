dda58653a8cfab3d4e429c3d223b60d0e6227b94
myra-suk <myrasanaa17@gmail.com>
2022-09-14 18:08:54 +0300

    Update 3-islower.c
    Task 3.1 for lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,9 +4,8 @@
  * _islower - Shows 1 if the input is a
  * lowercase character. Another cases, shows
  * 0
- * 
- * @c : The character in ASCII code
- * 
+ *
+ *
  * Return : 1 for lowercase character. 0 for the rest.
  */
 int _islower(int c)
