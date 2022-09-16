420f0178d3339543d5ee58d135abd8d5bc2f2765
myra-suk <myrasanaa17@gmail.com>
2022-09-17 02:24:34 +0300

    Create 8-print_square.c
    Task 8.Print hashes square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - prints hashes square
+ * @size: size of the square.
+ * Return: no return
+ */
+void print_square(int size)
+{
+int x, y;
+
+for (x = 0; x < size; x++)
+{
+for (y = 0; y < size; y++)
+{
+_putchar(35);
+}
+if (x != size - 1)
+_putchar('\n');
+}
+_putchar('\n');
+}
