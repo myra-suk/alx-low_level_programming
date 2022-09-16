0f58c6c1070507f348c9921d0ce5f5adeff4c982
myra-suk <myrasanaa17@gmail.com>
2022-09-16 17:59:33 +0300

    Create 1-main.c
    Task 1.Like, comment, subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main- causes an infite loop
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+
+printf("Infinite loop incoming :/\n");
+
+i = 0;
+
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
+
+printf("Infinite loop disscrded :D \\o/\n");
+
+return(0);
+}
