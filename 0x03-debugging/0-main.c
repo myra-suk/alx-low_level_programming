af599adda04ff7c8a720f9aaeb1655092aa0ed62
myra-suk <myrasanaa17@gmail.com>
2022-09-16 17:38:58 +0300

    0.main.c and main.h
    Task 0.Postitive or Negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main- tests funtions that determines whether integer is positive or negative
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
+}
