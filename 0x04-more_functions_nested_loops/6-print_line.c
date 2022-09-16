2b248934f34e1a268ddf93477dbbf8da23f89909
myra-suk <myrasanaa17@gmail.com>
2022-09-17 02:09:18 +0300

    Create 6-print_line.c
    Task 6.Print line n times

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line- prints straight line n times
+ * @n: number of times straight line is printed
+ * Return: no return
+ */
+void print_line(int n)
+{
+int m;
+
+for (m = 0; m < n; m++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
