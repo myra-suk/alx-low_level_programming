0bc17a6f01173bbce56f7646506bef562f97b893
myra-suk <myrasanaa17@gmail.com>
2022-09-20 21:22:27 +0300

    Create 9-strcpy.c
    Task 9.Copies the string pointer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy- Copies the string pointed to by src,
+ * including the terminatin null byte, to the 
+ * buffer pointed to by dest
+ * @dest: destination.
+ * @src: source.
+ * Return: The pointer to the destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+int counter = 0;
+
+while (counter >= 0)
+{
+*(dest + counter) = *(src + counter);
+if (*(src + counter) == '\0')
+break;
+counter++;
+}
+return (dest);
+}
