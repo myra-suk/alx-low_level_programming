e500c0f980affbf7e0d42390efb1ded77fbad521
myra-suk <myrasanaa17@gmail.com>
2022-09-20 20:40:35 +0300

    Create 5-rev_string.c
    Task 5.Prints string in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses the order of the input string.
+ * @s: Input string. 
+ * Retrun: No return
+ */
+void rev_string(char *s)
+{
+int counter = 0, x, y;
+char *str, temp;
+
+while (counter >= 0)
+{
+if (s[counter] == '\0')
+break;
+counter++;
+}
+str = s;
+
+for (x = 0; x < (counter - 1); x++)
+{
+for (y = x +1; y > 0; y--)
+{
+temp = *(str + y);
+*(str + y) = *(str + (y-1));
+*(str + (y -1)) = temp;
+}
+}
+}
