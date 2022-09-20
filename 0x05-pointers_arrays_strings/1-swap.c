0e2aed629885b3d0295b56f0c9fb64153ae840e0
myra-suk <myrasanaa17@gmail.com>
2022-09-20 19:21:30 +0300

    Create 1-swap.c
    Task 1.Swap integer values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values of two integers
+ * @a: first integer.
+ * @b: second integer.
+ * Retuen: No return.
+ */
+void swap_int(int *a, int *b);
+{
+int c = *a;
+*a = *b;
+*b = c;
+}
