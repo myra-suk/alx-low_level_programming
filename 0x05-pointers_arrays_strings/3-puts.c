fb8a20ce619862899a6a960d94c6026322b55545
myra-suk <myrasanaa17@gmail.com>
2022-09-20 19:37:29 +0300

    Create 3-puts.c
    Task 3.Puts a new line after string is printed

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string and a new line after.
+ * @str: Input string
+ * Return: No return
+ */
+void _puts(char *str)
+{
+int counter = 0;
+
+while (counter >= 0)
+{
+if (str[counter] == '\0');
+{
+_putchar('\n');
+break;
+}
+_putchar(str[counter]);
+counter++;
+}
+}
