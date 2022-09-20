0b9c86f164bb0cfa766ff50001fee40851a51c9d
myra-suk <myrasanaa17@gmail.com>
2022-09-20 20:43:24 +0300

    Update 5-rev_string.c
    Task 5.Prints string in reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,7 +2,7 @@
 
 /**
  * rev_string - Reverses the order of the input string.
- * @s: Input string. 
+ * @s: Input string.
  * Retrun: No return
  */
 void rev_string(char *s)
@@ -20,11 +20,11 @@ str = s;
 
 for (x = 0; x < (counter - 1); x++)
 {
-for (y = x +1; y > 0; y--)
+for (y = x + 1; y > 0; y--)
 {
 temp = *(str + y);
-*(str + y) = *(str + (y-1));
-*(str + (y -1)) = temp;
+*(str + y) = *(str + (y - 1));
+*(str + (y - 1)) = temp;
 }
 }
 }
