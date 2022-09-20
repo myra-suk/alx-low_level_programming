23f0a1e7354f55972eb2f5c90da45fdac1dc2d76
myra-suk <myrasanaa17@gmail.com>
2022-09-20 19:14:24 +0300

    0.Reset to 98
    Task 0.Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98- Takes a pointer to a parameter and updtes its value to 98.
+ * @n: Input integer.
+ * Return: No return.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
