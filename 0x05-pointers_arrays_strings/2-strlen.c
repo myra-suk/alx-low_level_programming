c09e54ff7df7caf7ac7f0cf7bde4165534eae588
myra-suk <myrasanaa17@gmail.com>
2022-09-20 19:29:12 +0300

    Create 2-strlen.c
    Tsk 2.Check length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen- Checks the program and returns the length of a string.
+ * @s: Input string.
+ * Return: length of a string.
+ */
+int _strlen(char *s)
+{
+int counter = 0;
+
+while (*(s + counter) != '\0')
+counter++;
+return(counter);
+}
