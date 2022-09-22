47d07bba7a85502b7c9565b53e9bde04980ac966
myra-suk <myrasanaa17@gmail.com>
2022-09-22 16:21:21 +0300

    Update 0-strcat.c
    Task 0.Links and joins two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,7 +4,7 @@
  * _strcat- Links and joing two strings
  * @dest: destination.
  * @src: source.
- * Return: the pointer to the destination.
+ * Return: the pointer to dest. 
  */
 char *_strcat(char *dest, char *src)
 {
