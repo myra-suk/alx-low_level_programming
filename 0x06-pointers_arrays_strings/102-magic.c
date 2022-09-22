10714031b3f69256a049deeede19fa914294f270
myra-suk <myrasanaa17@gmail.com>
2022-09-22 19:08:23 +0300

    Create 102-magic.c
    Task 10.Magic

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+
+int main (void)
+{
+int x;
+int a[5];
+int *p;
+
+a[2] = 1024;
+p = &x;
+
+p[5] = 98;
+
+printf("a[2] = %d\n", a[2]);
+return (0);
+}
