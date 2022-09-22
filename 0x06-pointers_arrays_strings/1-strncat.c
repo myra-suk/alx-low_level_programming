a879fad7a6ebc73ce077c8de9d12f0b471677ac7
myra-suk <myrasanaa17@gmail.com>
2022-09-22 16:51:03 +0300

    Create 1-strncat.c
    Task 1. Concatenates two strings with n bytes from src

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat- Concatenates two strings 
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes from src.
+ * Return: the pointer to dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int counter = 0, counter2 = 0;
+
+while (*(dest + counter) != '\0')
+{
+counter++;
+}
+
+while (counter2 < n)
+{
+*(dest + counter) = *(src + counter2);
+if (*(src + counter2) == '\0')
+break;
+counter++;
+counter2++;
+}
+return (dest);
+}
