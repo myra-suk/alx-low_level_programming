e1e7479bb6be078796b4eb4a4523c8a8cd6e1d6c
myra-suk <myrasanaa17@gmail.com>
2022-09-22 19:16:59 +0300

    Update 103-infinite_add.c
    Task 11.Infinite Add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * infinite_add - adds two numbers
+ * infinite_add- adds two numbers.
  * @n1: number one.
  * @n2: number two.
  * @r: buffer that the function will use to store the result.
