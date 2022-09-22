5ddad58bd84b099ae3f9e6be58ead7bc1f62adcd
myra-suk <myrasanaa17@gmail.com>
2022-09-22 18:41:57 +0300

    Update 7-leet.c
    Task 7.Encodes string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,28 +2,29 @@
 
 /**
  * leet- Encodes  string into 1337
- * @s: Input string
+ * @s: Input string.
  * Return: the pointer to dest.
  */
 
 char *leet(char *s)
 {
-int counter = 0, x;
+int counter = 0, i;
 int low_letters[] = {97, 101, 111, 116, 108};
 int upp_letters[] = {65, 69, 79, 84, 76};
 int numbers[] = {52, 51, 48, 55, 49};
 
 while (*(s + counter) != '\0')
 {
-for (x = 0; x < 5; x++)
+for (i = 0; i < 5; i++)
 {
-if (*(s + counter) == low_letters[x] || *(s + counter) == upp_letters[x])
+if (*(s + counter) == low_letters[i] || *(s + counter) == upp_letters[i])
 {
-*(s + counter) = numbers[x];
+*(s + count) = numbers[i];
 break;
 }
 }
 counter++;
 }
-return(s);
+
+return (s);
 }
