978f7d601c7272f62c3880524ddbc7275f107fb2
myra-suk <myrasanaa17@gmail.com>
2022-09-22 16:59:55 +0300

    Create 2-strncpy.c
    Task 2. Copies a string with n bytes from src

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy- Copies a string.
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes from src.
+ * Return: the pointer to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int x;
+
+for (x = 0; x < n && src[x] != '\0'; x++)
+dest[x] = src[x];
+for(; x < n; x++)
+dest[x] = '\0';
+
+return (dest);
+}
