aaa652787e25bfcb555a5bcadd2dc9ad51134a86
myra-suk <myrasanaa17@gmail.com>
2022-09-22 16:09:53 +0300

    0. _strcat.c
    Task 0.Links and joins two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - Links and joing two strings,
+ * @dest: destination.
+ * @src: source.
+ * Return: the pointer to the destination.
+ */
+char *_strcat(char *dest, char *src)
+{
+int counter = 0, counter2 = 0;
+
+while (*(dest + counter) != '\0')
+{
+counter++;
+}
+
+while (counter2 >= 0)
+{
+*(dest + counter) = *(src +counter2);
+if (*(src + counter2) == '\0')
+break;
+counter++;
+counter2++;
+}
+return (dest);
+}
