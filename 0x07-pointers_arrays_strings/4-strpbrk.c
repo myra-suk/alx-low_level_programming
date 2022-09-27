ef96b288b0a8e5cd5f77e223440d21b111619ac3
myra-suk <myrasanaa17@gmail.com>
2022-09-28 00:50:45 +0300

    Update 4-strpbrk.c
    Task 4.Searches a string for any sets of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,7 +4,7 @@
  * _strpbrk- Searches a string for any of a set of bytes.
  * @s: First string.
  * @accept: Second string.
- * Return: A pointer to the byte that matches one of the 
+ * Return: A pointer to the byte that matches one of the
  * bytes in accept, or NULL if no matching byte is found
  */
 char *_strpbrk(char *s, char *accept)
