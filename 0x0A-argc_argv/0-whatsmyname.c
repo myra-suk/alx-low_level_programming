b7dcc50ca9dfc349878ef557a10f0a2257a471c6
myra-suk <myrasanaa17@gmail.com>
2022-09-30 17:32:57 +0300

    Update 0-whatsmyname.c
    Task 0. Prints its name, followed by  new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main- Prints the name, followed by a new line
+ * main- Prints its name, followed by a new line.
  * @argc: Number of command line arguments.
  * @argv: Array that contains the program command line arguments.
- * Return: 0- Success.
+ * Return: 0- success.
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
